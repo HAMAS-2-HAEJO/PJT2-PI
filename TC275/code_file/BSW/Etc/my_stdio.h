59b4a3f8c1b637ccf44ebe3aee5045df00286ba4
kkimd0 <146160294+kkimd0@users.noreply.github.com>
2023-11-24 17:55:58 +0900

    파일 이동 3

---
+++ TC275/code_file/BSW/Etc/my_stdio.h
@@ -0,0 +1,9 @@
+#ifndef BSW_ETC_MY_STDIO_H_
+#define BSW_ETC_MY_STDIO_H_
+
+extern void Init_Mystdio(void);
+extern void my_puts(const char *str);
+extern int my_printf(const char *format, ...);
+extern void my_scanf(const char *fmt, ...);
+
+#endif /* BSW_ETC_MY_STDIO_H_ */
